03664b9a6e3ee164da232c53d0e64274df3566a2
Thiago Medeiros <thiagomunhozmedeiros@gmail.com>
2022-12-25 19:10:29 -0300

    feat(protocol): add http-client

---
+++ core/data/protocols/http-client.ts
@@ -0,0 +1,27 @@
+export type HttpMethod = 'post' | 'get' | 'put' | 'delete'
+
+export type HttpRequest = {
+  url: string
+  method: HttpMethod
+  body?: any
+  headers?: any
+}
+
+export enum HttpStatusCode {
+  ok = 200,
+  noContent = 204,
+  badRequest = 400,
+  unauthorized = 401,
+  forbidden = 403,
+  notFound = 404,
+  serverError = 500
+}
+
+export type HttpResponse<T = any> = {
+  statusCode: HttpStatusCode
+  body: T
+}
+
+export interface HttpClient<T = any> {
+  request: (data: HttpRequest) => Promise<HttpResponse<T>>
+}
