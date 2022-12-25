0c9d8e44cea88f873f92b5cb7e24f025e796a098
Thiago Medeiros <thiagomunhozmedeiros@gmail.com>
2022-12-25 19:11:10 -0300

    test: add mock-http

---
+++ core/data/test/mock-http.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpRequest, HttpResponse, HttpStatusCode } from "../protocols/http-client"
+
+export class HttpClientSpy<T = any> implements HttpClient<T> {
+  url?: string
+  method?: string
+  body?: any
+  headers?: any
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.ok,
+    body: {}
+  }
+
+  async request(params: HttpRequest): Promise<HttpResponse<T>> {
+    this.url = params.url
+    this.method = params.method
+    this.body = params.body
+    this.headers = params.headers
+    return this.response
+  }
+}
